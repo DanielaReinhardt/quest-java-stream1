353c4696f232582c1864f87996bf0f099e795631
DanielaReinhardt <reinhardtd@telekom.de>
2022-04-25 14:49:39 +0200

    Stream

--- Shield.java
+++ Shield.java
@@ -17,20 +17,44 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
+        List<Hero> elders = new ArrayList<>();
         // TODO 1 : filter heroes in order to found heroes older than 59
+        //Theoretisch richttig, aber ist kein Filter bzw. Stream:
+        // for (Hero elders2 : heroes) {
+        //     if (elders2.getAge() > 59) {
+        //         elders.add(elders2);
+        //     }
+        // }
+        Predicate <Hero> older = p ->p.getAge()>59;
+        elders = heroes.stream().filter(older).collect(Collectors.toList());    
 
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
-
-        List<Hero> intolerants;
+             
+        List<Hero> intolerants = new ArrayList<>();
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
 
-        System.out.println("\nGluten intolerants:");
-        for (Hero intolerant : intolerants) {
+        Predicate <Hero> gluten = p ->p.isGlutenIntolerant()==true;
+        intolerants = heroes.stream().filter(gluten)
+                                     .collect(Collectors.toList());
+
+        //ohne das komische Predicate
+        // Predicate <Hero> gluten = p ->p.isGlutenIntolerant()==true;
+        // intolerants = heroes.stream().filter(intolerant -> intolerant.isGlutenIntolerant() == true)
+        //                              .collect(Collectors.toList());
+    
+        System.out.println("\nintolerant:");
+
+        for (Hero intolerant : intolerants)
             System.out.println(intolerant.getName());
-        }
+
+        //in Lambda (fügt die for-each-Schleife mit sysout zusammen):
+        //intolerants.forEach((intol) -> System.out.println(intol.getName()));
+            
+    
     }
+
 }
+
